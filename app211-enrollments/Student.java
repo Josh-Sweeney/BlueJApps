f9051ddd6c630b735eaa377439dd74b2334b14e3
Josh-Sweeney <91318891+Josh-Sweeney@users.noreply.github.com>
2021-09-24 12:49:07 +0100

    Add screenshot, log output & edit modified by comment

--- app211-enrollments/Student.java
+++ app211-enrollments/Student.java
@@ -4,7 +4,7 @@
  * concerning the course the student is currently enrolled on
  * 
  * @author Michael Kölling and David Barnes
- * @modified by Derek Peacock and Nicholas Day
+ * @modified by Joshua Sweeney
  * @version 2021:08:15
  */
 public class Student

c1dfc3aeecca2ec2ccd4f69308d4be764dbf2b92
Josh-Sweeney <91318891+Josh-Sweeney@users.noreply.github.com>
2022-01-21 12:02:01 +0000

    Add remaining item types

--- app216-zuul/ItemType.java
+++ app216-zuul/ItemType.java
@@ -2,10 +2,12 @@
  * A enumerator containing a list of all the
  * items in the game
  * @author Joshua Sweeney
- * @version 07-02-22
+ * @version 21-01-22
  */
 public enum ItemType
 {
 	Torch,
-	Coin
+	Coin,
+	Gold,
+	Flowers
 }

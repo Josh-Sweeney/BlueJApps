56c314d1f8b3e00bbdf1f3010726716be1904eca
Josh-Sweeney <91318891+Josh-Sweeney@users.noreply.github.com>
2022-01-27 16:22:30 +0000

    Fix findInventoryItem function

--- app216-zuul/Player.java
+++ app216-zuul/Player.java
@@ -7,60 +7,60 @@ import java.util.ArrayList;
  */
 public class Player
 {
-	// Health
-	public int health;
+    // Health
+    public int health;
 
-	// Score
-	public int score;
+    // Score
+    public int score;
 
-	// Inventory
-	public ArrayList<Item> inventory;
+    // Inventory
+    public ArrayList<Item> inventory;
 
-	/**
-	 * Creates a new player with an initial
-	 * health and score
-	 * @param health the initial health of the
-	 * player
-	 * @param score the initial score of the 
-	 * plaer
-	 */
+    /**
+     * Creates a new player with an initial
+     * health and score
+     * @param health the initial health of the
+     * player
+     * @param score the initial score of the 
+     * plaer
+     */
     public Player(int health, int score)
     {
         this.health = health;
-		this.score = score;
-		inventory = new ArrayList<Item>();
+        this.score = score;
+        inventory = new ArrayList<Item>();
     }
 
-	/**
-	 * Attempts to find an item from the player's
-	 * inventory based on its name
-	 * @param name the name of the item to search
-	 * for
-	 * @return the found item. If not found null
-	 * is returned
-	 */
-	public Item findInventoryItem(String name)
-	{
-		// Iterate over every item in the inventory
-		for (Item item : this.inventory)
-		{
-			// Return the current item if the name matches
-			if (item.getName() == name)
-			{		
-				return item;
-			}
-		}
-		
-		// If not found, return null
-		return null;
-	}
+    /**
+     * Attempts to find an item from the player's
+     * inventory based on its name
+     * @param name the name of the item to search
+     * for
+     * @return the found item. If not found null
+     * is returned
+     */
+    public Item findInventoryItem(String name)
+    {
+        // Iterate over every item in the inventory
+        for (Item item : this.inventory)
+        {
+            // Return the current item if the name matches
+            if (item.getName().equals(name))
+            {		
+                return item;
+            }
+        }
 
-	/**
-	 * Adds an item to the current player's inventory
-	 * @param item The item to add to the inventory
-	 */
-	public void inventoryAdd(Item item) 
-	{
-		this.inventory.add(item);
-	}
+        // If not found, return null
+        return null;
+    }
+
+    /**
+     * Adds an item to the current player's inventory
+     * @param item The item to add to the inventory
+     */
+    public void inventoryAdd(Item item) 
+    {
+        this.inventory.add(item);
+    }
 }
